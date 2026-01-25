a7702eb7fbe84a1a0f87a31331446383bafcfaed
Ryuzaki5100 <atharvaa2210@gmail.com>
2026-01-25 16:22:51 +0530

    Updated Flake, changed ui.nix to include 100s of different colorschemes

--- plugins/ui.nix
+++ plugins/ui.nix
@@ -1,6 +1,9 @@
 # nixvim-config/plugins/ui.nix
-{ ... }:
+{ lib, ... }:
 {
+  # Force the default colorscheme so it wins over modules that set the option themselves
+  colorscheme = lib.mkForce "gruvbox-material";
+
   plugins = {
     lualine.enable = true;
 
@@ -30,9 +33,100 @@
     };
   };
 
+  # Enable all themes so you can switch with :colorscheme <name>
+  colorschemes.ayu = {
+    enable = true;
+  };
+  colorschemes.bamboo = {
+    enable = true;
+  };
+  colorschemes.base16 = {
+    enable = true;
+  };
+  colorschemes.catppuccin = {
+    enable = true;
+  };
+  colorschemes.cyberdream = {
+    enable = true;
+  };
+  colorschemes.dracula = {
+    enable = true;
+  };
+  colorschemes.dracula-nvim = {
+    enable = true;
+  };
+  colorschemes.everforest = {
+    enable = true;
+  };
+  colorschemes.github-theme = {
+    enable = true;
+  };
+  colorschemes.gruvbox = {
+    enable = true;
+  };
+  colorschemes.gruvbox-baby = {
+    enable = true;
+  };
   colorschemes.gruvbox-material = {
     enable = true;
     settings.transparent_background = 2;
+  };
+  colorschemes.gruvbox-material-nvim = {
+    enable = true;
+  };
+  colorschemes.kanagawa = {
+    enable = true;
+  };
+  colorschemes.kanagawa-paper = {
+    enable = true;
+  };
+  colorschemes.melange = {
+    enable = true;
+  };
+  colorschemes.modus = {
+    enable = true;
+  };
+  colorschemes.monokai-pro = {
+    enable = true;
+  };
+  colorschemes.moonfly = {
+    enable = true;
+  };
+  colorschemes.nightfox = {
+    enable = true;
+  };
+  colorschemes.nord = {
+    enable = true;
+  };
+  colorschemes.one = {
+    enable = true;
+  };
+  colorschemes.onedark = {
+    enable = true;
+  };
+  colorschemes.oxocarbon = {
+    enable = true;
+  };
+  colorschemes.palette = {
+    enable = true;
+  };
+  colorschemes.poimandres = {
+    enable = true;
+  };
+  colorschemes.rose-pine = {
+    enable = true;
+  };
+  colorschemes.solarized-osaka = {
+    enable = true;
+  };
+  colorschemes.tokyonight = {
+    enable = true;
+  };
+  colorschemes.vague = {
+    enable = true;
+  };
+  colorschemes.vscode = {
+    enable = true;
   };
 
   keymaps = [
