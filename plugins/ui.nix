c3649ac1eac27c94729f4d8b5fed5a73f69584a0
Ryuzaki5100 <atharvaa2210@gmail.com>
2026-06-19 19:36:44 +0530

    Find the Highest Altitude

--- plugins/ui.nix
+++ plugins/ui.nix
@@ -3,9 +3,10 @@
 {
   # Force the default colorscheme so it wins over modules that set the option themselves
   # colorscheme = lib.mkForce "gruvbox";
-  colorscheme = lib.mkForce "kanagawa-dragon";
+  # colorscheme = lib.mkForce "kanagawa-dragon";
   # colorscheme = lib.mkForce "base16-vesper";
   # colorscheme = lib.mkForce "base16-darkviolet";
+  colorscheme = lib.mkForce "base16-rose-pine";
 
   plugins = {
     lualine.enable = true;
